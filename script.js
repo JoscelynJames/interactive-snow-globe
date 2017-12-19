e1292a498067847d47962d727465dfc35816eeb6
Joscelyn James <josce.james7@hotmail.com>
2017-12-19 15:38:18 -0700

    it shakes

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+function shake() {
+	var shakeTimeline = anime.timeline();
+	var tiltRight = {
+		targets: 'svg',
+		rotate: {
+			value: 30,
+			duration: 300,
+			easing: 'easeInOutCirc'
+		},
+	}
+
+	var tileLeft = {
+		targets: 'svg',
+		rotate: {
+			value: -30,
+			duration: 300,
+			easing: 'easeInOutCirc'
+		},
+	}
+
+	shakeTimeline
+		.add(tiltRight)
+		.add(tileLeft)
+		.add(tiltRight)
+		.add(tileLeft)
+		.add({
+			targets: 'svg',
+			rotate: {
+				value: 0,
+				duration: 300,
+				easing: 'easeInOutCubic'
+			},
+		})
+}
 
